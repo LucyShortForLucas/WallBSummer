26b0917dc16e7b2830f2d804fdce38e1b6863984
Thomas Deroy <thooommy5@gmail.com>
2026-08-20 03:06:34 +0200

    Adding aggression meter and making the movement better of the robots

--- Project/Prototype/Assets/Scripts/Gameloop/EnemyHandler.cs
+++ Project/Prototype/Assets/Scripts/Gameloop/EnemyHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHandler : MonoBehaviour, IInjectable
@@ -14,7 +15,15 @@ public class EnemyHandler : MonoBehaviour, IInjectable
     [SerializeField] private LayerMask alliesLayer;
     [SerializeField] private float safeDistance = 30f;
 
+    [Header("Hive Memory")]
+    [SerializeField] private int globalAggression = 0;
+    private List<Vector3> fallenLeaderPositions = new List<Vector3>();
+    private readonly int aggressionThreshold = 5;
+
     private TimeManager timeManager;
+
+    // Getters and Setters
+    public int GlobalAggression { get => globalAggression; }
 
     public void Inject(DependencyContainer container)
     {
@@ -57,17 +66,22 @@ public class EnemyHandler : MonoBehaviour, IInjectable
 
         Debug.Log($"Wave timeeeeeee, Spawning {leaderCount} Leaders with {minionsPerLeader} minions each");
 
+        Vector3? revengePos = GetRevengeTarget();
+
         for (int i = 0; i < leaderCount; i++)
         {
             Vector3 spawnPos = FindSafeSpawnPosition();
-            Instantiate(leaderPrefab, spawnPos, Quaternion.identity);
+            GameObject leaderObj = Instantiate(leaderPrefab, spawnPos, Quaternion.identity);
+
+            leaderObj.GetComponent<LeaderRobot>().SetupMemory(this, revengePos);
 
             for (int m = 0; m < minionsPerLeader; m++)
             {
                 Vector2 randomCircle = Random.insideUnitCircle * 5f;
                 Vector3 minionPos = spawnPos + new Vector3(randomCircle.x, 0, randomCircle.y);
 
-                Instantiate(minionPrefab, minionPos, Quaternion.identity);
+                GameObject minionObj = Instantiate(minionPrefab, minionPos, Quaternion.identity);
+                minionObj.GetComponent<BaseRobotAI>().WaveHandler = this;
             }
         }
     }
@@ -89,4 +103,41 @@ public class EnemyHandler : MonoBehaviour, IInjectable
 
         return new Vector3(spawnMapRadius, 0, 0);
     }
+
+    public void IncreaseAggression(int amount)
+    {
+        globalAggression += amount;
+        Debug.Log($"Aggression increased to {globalAggression}");
+    }
+
+    public void RecordLeaderDeath(Vector3 deathPosition)
+    {
+        fallenLeaderPositions.Add(deathPosition);
+    }
+
+    public Vector3? GetRevengeTarget()
+    {
+        // 50% chance to investigate a past death site if one exists
+        if (fallenLeaderPositions.Count > 0 && Random.value > 0.5f)
+        {
+            return fallenLeaderPositions[Random.Range(0, fallenLeaderPositions.Count)];
+        }
+        return null;
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (fallenLeaderPositions == null || fallenLeaderPositions.Count == 0) return;
+
+        Gizmos.color = Color.red;
+        foreach (Vector3 pos in fallenLeaderPositions)
+        {
+            Gizmos.DrawSphere(pos, 1.5f);
+            Gizmos.DrawLine(pos - Vector3.up * 2, pos + Vector3.up * 2);
+            Gizmos.DrawLine(pos - Vector3.right * 2, pos + Vector3.right * 2);
+            Gizmos.DrawLine(pos - Vector3.forward * 2, pos + Vector3.forward * 2);
+        }
+    }
+#endif
 }
